740c3f95aabcd615ab16091569b97b08741b13c2
sachinmeena1994 <skillsuup@gmail.com>
2024-12-20 12:16:36 +0530

    added slider compoent

---
+++ src/Components/ImageSlider/index.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+
+const ImageSlider = ({ images }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  // Function to go to the next slide
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  // Function to go to the previous slide
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  // Auto-slide functionality (every 5 seconds)
+  useEffect(() => {
+    const interval = setInterval(nextSlide, 5000);
+    return () => clearInterval(interval); // Cleanup on component unmount
+  }, [currentIndex]);
+
+  return (
+    <div className="relative w-full h-screen overflow-hidden">
+      {/* Images */}
+      <div
+        className="flex transition-transform duration-500"
+        style={{
+          transform: `translateX(-${currentIndex * 100}%)`,
+        }}
+      >
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className="min-w-full h-screen bg-center bg-cover"
+            style={{ backgroundImage: `url(${image})` }}
+          ></div>
+        ))}
+      </div>
+
+      {/* Previous Button */}
+      <button
+        onClick={prevSlide}
+        className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-black/50 text-white p-2 rounded-full hover:bg-black/70 focus:outline-none"
+      >
+        &#10094;
+      </button>
+
+      {/* Next Button */}
+      <button
+        onClick={nextSlide}
+        className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-black/50 text-white p-2 rounded-full hover:bg-black/70 focus:outline-none"
+      >
+        &#10095;
+      </button>
+
+      {/* Dots for navigation */}
+      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
+        {images.map((_, index) => (
+          <button
+            key={index}
+            onClick={() => setCurrentIndex(index)}
+            className={`w-3 h-3 rounded-full ${
+              index === currentIndex ? "bg-black" : "bg-gray-400"
+            }`}
+          ></button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ImageSlider;
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,11 +1,26 @@
 import React from "react";
+import ImageSlider from "../../Components/ImageSlider";
 
-function Home() {
+const images = [
+  "https://images.pexels.com/photos/11838557/pexels-photo-11838557.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  "https://images.pexels.com/photos/13621067/pexels-photo-13621067.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  "https://images.pexels.com/photos/20975666/pexels-photo-20975666/free-photo-of-aerial-view-of-the-monument-portal-de-la-costa-brava-on-the-beach-in-blanes-spain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+];
+
+const Home = () => {
   return (
-    <div className="h-screen flex items-center justify-center bg-white text-black text-4xl font-extrabold">
-      Welcome to India Bhavan!
-    </div>
+    <>
+      <ImageSlider images={images} />
+      <div className="p-6">
+        <h1 className="text-4xl font-bold text-center">
+          Welcome to the Home Page
+        </h1>
+        <p className="text-lg mt-4 text-center">
+          Scroll down to see more content!
+        </p>
+      </div>
+    </>
   );
-}
+};
 
 export default Home;
